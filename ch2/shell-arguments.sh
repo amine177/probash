968a76605c0e2d62cd3b850e5f5aeea2dab23b2e
aminos <amri.amine@protonmail.com>
2019-05-31 23:12:19 +0100

    illustrating printing argument using $1

--- ch2/shell-arguments.sh
+++ ch2/shell-arguments.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
-
-printf "my first argument is %s\n" "$1";
-exit 0;
+if [ -z $1 ]
+then
+  echo "Please give me an argument!";
+  exit 1;
+else
+  printf "my first argument is %s\n" "$1";
+  exit 0;
+fi;
